fe148a34418f0718fefdbf74ecdbdaa1488e9317
Alex Raccuglia <shylock74@gmail.com>
2026-04-22 10:03:47 +0200

    Initial Commit

---
+++ ImaJoin/ImaJoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImaJoinApp.swift
+//  ImaJoin
+//
+//  Created by Alex Raccuglia on 22/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct ImaJoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
